a936052db8ba7c4ae84fb4c4e0d74046d132e093
Jessica Jimenez <jjimenez174@yahoo.com>
2024-04-09 21:22:27 -0700

    pushing to github

---
+++ models/task.js
@@ -0,0 +1,61 @@
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Task extends Model {}
+
+Task.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    created_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    due_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    priority: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    project_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "project",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "task",
+  }
+);
+
+module.exports = Task;
---
+++ public/js/login.js
@@ -0,0 +1,39 @@
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the main page
+      Swal.fire({
+        text: "Mr Get It Done is ready for ya...",
+        icon: "success",
+        background: "white",
+        confirmButtonText: "Go",
+      }).then((result) => {
+        document.location.replace("/homepage");
+      });
+    } else {
+      Swal.fire({
+        text: "Login failed",
+        icon: "error",
+        background: "white",
+        confirmButtonText: "Ok",
+      });
+    }
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
